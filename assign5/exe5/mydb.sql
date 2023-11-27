a565dde8df2a35cffd81a634a9dcc616e03faaf3
rutvijapatil <rutvijapatil>
2023-11-28 00:00:08 +0530

    first commit

---
+++ assign5/exe5/mydb.sql
@@ -0,0 +1,7 @@
+create table company(id integer, name varchar(100));
+
+insert into company (id, name) values (1, 'person1');
+insert into company (id, name) values (2, 'person2');
+insert into company (id, name) values (3, 'person3');
+insert into company (id, name) values (4, 'person4');
+insert into company (id, name) values (5, 'person5');
